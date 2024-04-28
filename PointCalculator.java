5fb9f6b71b48c679f0266129086200bdc2275845
Adam Behun <adam.behun@slu.edu>
2024-04-28 16:45:08 -0500

    PointCalculator first version, will add better getLetterValue

--- PointCalculator.java
+++ PointCalculator.java
@@ -2,3 +2,20 @@
  * Calculates the scores for words placed on the board
  * Understand different scoring multipliers 
 */ 
+
+public class PointCalculator{
+	
+	// how to do multipliers?
+	public int calculatePoints(String word, int[] multipliers) {
+		int score = 0;
+		for (int i = 0; i < word.length(); i++) {
+			score += getLetterValue(word.charAt(i)) * multiplier[i];
+		}
+		return score;
+	}
+	
+	private int getLetterValue(char letter) {
+		// preliminary
+		return 1;
+	}
+}
