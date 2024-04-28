8d2d4cd92ca5aeee848025fbde4cfd6089216be7
Adam Behun <adam.behun@slu.edu>
2024-04-28 16:37:49 -0500

    first v. of BagOfLetters, will need to do initialize bag

--- BagOfLetters.java
+++ BagOfLetters.java
@@ -2,3 +2,27 @@
  * All available letters in the game
  * Checks if bag empty and allows to draw letters
 */ 
+
+import java.util.ArrayList;
+
+public class BagOfLetters{
+	private ArrayList<Character> letters;
+	
+	public BagOfLetters() {
+		this.letters = new ArrayList<>();
+		
+		// initialize the bag of letters
+		// then shuffle
+	}
+	
+	public char pullLetter() {
+		if (!letters.isEmpty()) {
+			return letters.remove(0);
+		}
+		return '\0'; 
+	}
+	
+	public boolean isEmpty() {
+		return letters.isEmpty();
+	}
+}
