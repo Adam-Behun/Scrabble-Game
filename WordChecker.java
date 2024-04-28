0a3b34bed22b1a1e882807af93cf116d10a385c9
Adam Behun <adam.behun@slu.edu>
2024-04-28 17:05:22 -0500

    simplest v. of WordChecker, will need to connect to dictionary algorithm

--- WordChecker.java
+++ WordChecker.java
@@ -2,3 +2,23 @@
  * Validates the words formed against the dictionary
  * Ensures that only valid words are used in the game
 */ 
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordChecker{
+	private List<String> validWords;
+	
+	public WordChecker() {
+		this.validWords = new ArrayList<>();
+		
+		// few sample words if we need them
+		validWords.add("example");
+		validWords.add("board--12");
+		validWords.add("Game");
+	}
+	
+	public boolean isValidWord(String word) {
+		return validWords.contains(word.toLowerCase());
+	}
+}
